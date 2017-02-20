c87e7f7ae10d3968c236beae905e27bc6d145492
alan.yang <jpra1113@gmail.com>
2017-02-21 00:04:12 +0800

    1.update retryConnectSSH
    2.setupK8S add deploy service
    
    3.add deleteDeployment

--- kubernetes/deploy.go
+++ kubernetes/deploy.go
@@ -19,6 +19,7 @@ import (
 	"github.com/aws/aws-sdk-go/service/iam"
 
 	k8s "k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/pkg/api/v1"
 	"k8s.io/client-go/rest"
 )
 
@@ -43,33 +44,38 @@ func waitUntilMasterReady(publicDNSName string) error {
 		Host: publicDNSName + ":8080",
 	}
 
+	var k8sError error
 	for !masterReady {
 		if _, err := k8s.NewForConfig(config); err != nil {
-			return errors.New("could not connect to Kubernetes API: " + err.Error())
+			k8sError = errors.New("Could not connect to Kubernetes API: " + err.Error())
+			time.Sleep(time.Duration(3) * time.Second)
 		} else if err == nil {
-			// TODO other condition
-			break
+			return nil
 		}
-		time.Sleep(time.Duration(3) * time.Second)
+
 	}
 	glog.Info("Master reading...")
 
-	return nil
+	return k8sError
 }
 
 func retryConnectSSH(address string, config *ssh.ClientConfig) (*common.SshClient, error) {
-	retryTimes := 5
+	// maxRetries is the number of times a SSH connect.
+	maxRetries := 5
 	sshClient := common.NewSshClient(address, config)
-	for i := 1; i <= retryTimes; i++ {
+
+	var sshError error
+	for i := 1; i <= maxRetries; i++ {
 		if err := sshClient.Connect(); err != nil {
-			return nil, errors.New("Unable to connect to server " + address + ": " + err.Error())
+			sshError = errors.New("Unable to connect to server: " + err.Error())
+			glog.Infof("retryConnectSSH %d", i)
+			time.Sleep(time.Duration(5) * time.Second)
 		} else if err == nil {
-			break
+			return &sshClient, nil
 		}
-		time.Sleep(time.Duration(5) * time.Second)
 	}
 
-	return &sshClient, nil
+	return &sshClient, sshError
 }
 
 func installKubernetes(deployedCluster *awsecs.DeployedCluster) error {
@@ -152,6 +158,45 @@ func CreateDeployment(viper *viper.Viper, deployment *apis.Deployment, uploadedF
 	return nil
 }
 
+// DeleteDeployment clean up the cluster from kubenetes.
+func DeleteDeployment(deployedCluster *awsecs.DeployedCluster) error {
+	config := &rest.Config{
+		Host: deployedCluster.NodeInfos[1].PublicDnsName + ":8080",
+	}
+
+	if c, err := k8s.NewForConfig(config); err == nil {
+		deploys := c.Extensions().Deployments(namespace)
+		if deployLists, listError := deploys.List(v1.ListOptions{}); listError == nil {
+			for _, deployment := range deployLists.Items {
+				deploys.Delete(deployment.GetObjectMeta().GetName(), &v1.DeleteOptions{})
+			}
+		}
+
+		replicaSets := c.Extensions().ReplicaSets(namespace)
+		if replicaSetLists, listError := replicaSets.List(v1.ListOptions{}); listError == nil {
+			for _, replicaSet := range replicaSetLists.Items {
+				replicaSets.Delete(replicaSet.GetObjectMeta().GetName(), &v1.DeleteOptions{})
+			}
+		}
+
+		pods := c.CoreV1().Pods(namespace)
+		if podLists, listError := pods.List(v1.ListOptions{}); listError == nil {
+			for _, pod := range podLists.Items {
+				pods.Delete(pod.GetObjectMeta().GetName(), &v1.DeleteOptions{})
+			}
+		}
+
+		services := c.CoreV1().Services(namespace)
+		if serviceLists, listError := services.List(v1.ListOptions{}); listError == nil {
+			for _, service := range serviceLists.Items {
+				services.Delete(service.GetObjectMeta().GetName(), &v1.DeleteOptions{})
+			}
+		}
+	}
+
+	return nil
+}
+
 func setupK8S(deployment *apis.Deployment, deployedCluster *awsecs.DeployedCluster) error {
 	config := &rest.Config{
 		Host: deployedCluster.NodeInfos[1].PublicDnsName + ":8080",
@@ -176,7 +221,16 @@ func setupK8S(deployment *apis.Deployment, deployedCluster *awsecs.DeployedClust
 			if err != nil {
 				return fmt.Errorf("could not create deployment controller: %s", err)
 			}
-			glog.Info("deployment controller created")
+			glog.Infof("%s deployment created", family)
+
+			if Kubernetes.Service.Kind == "Service" {
+				service := c.Core().Services(namespace)
+				_, err = service.Create(&Kubernetes.Service)
+				if err != nil {
+					return fmt.Errorf("failed to create service: %s", err)
+				}
+				glog.Infof("%s service created", Kubernetes.Service.GetObjectMeta().GetName())
+			}
 		}
 	}
 
