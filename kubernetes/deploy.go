61c6a2a2ac8ec0629b5de2229c5dd69e7cb7f37f
adrianliaw <adrianliaw2000@gmail.com>
2017-03-24 03:10:13 +0800

    Fix task counter

--- kubernetes/deploy.go
+++ kubernetes/deploy.go
@@ -472,6 +472,7 @@ func (k8sDeployment *KubernetesDeployment) deployServices() error {
 				return fmt.Errorf("Unable to find node id %s in cluster", mapping.Id)
 			}
 
+			originalFamily := family
 			count, ok := taskCount[family]
 			if !ok {
 				count = 1
@@ -487,7 +488,7 @@ func (k8sDeployment *KubernetesDeployment) deployServices() error {
 				deploySpec.Spec.Selector.MatchLabels = newLabels
 				deploySpec.Spec.Template.GetObjectMeta().SetLabels(newLabels)
 			}
-			taskCount[family] = count
+			taskCount[originalFamily] = count
 
 			// Assigning Pods to Nodes
 			nodeSelector := map[string]string{}
