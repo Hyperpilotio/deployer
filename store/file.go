486d1aae1fcc2b6da9509ecee7b713acfc8ab7d9
alan.yang <jpra1113@gmail.com>
2017-04-20 11:55:26 +0800

    Refactor file store

--- store/file.go
+++ store/file.go
@@ -17,41 +17,27 @@ type File struct {
 	Path string
 }
 
-func NewFile(config *viper.Viper) *File {
-	depStatPath := path.Join(config.GetString("filesPath"), "FileDeployment")
+func NewFile(config *viper.Viper) (*File, error) {
+	depStatPath := path.Join(config.GetString("filesPath"), "Deployment")
 	return &File{
 		Path: depStatPath,
-	}
+	}, nil
 }
 
 func (file *File) StoreNewDeployment(deployment *StoreDeployment) error {
+	deployInfos, err := file.getDeployInfos()
+	if err != nil {
+		return fmt.Errorf("Unable to get deployment info: %s", err.Error())
+	}
+	deployInfos[deployment.Name] = deployment
+
+	deployments := []*StoreDeployment{}
+	for _, deployInfo := range deployInfos {
+		deployments = append(deployments, deployInfo)
+	}
+
 	fileDeployment := &FileDeployment{
-		Deployments: []*StoreDeployment{},
-	}
-
-	if _, err := os.Stat(file.Path); err == nil {
-		deployments, err := file.LoadDeployment()
-		if err != nil {
-			return fmt.Errorf("Unable to load deployment status: %s", err.Error())
-		}
-		fileDeployment.Deployments = deployments
-	}
-
-	deployInfos := map[string]*StoreDeployment{}
-	for _, deployment := range fileDeployment.Deployments {
-		deployInfos[deployment.Name] = deployment
-	}
-
-	newDeployments := []*StoreDeployment{}
-	_, ok := deployInfos[deployment.Name]
-	if ok {
-		deployInfos[deployment.Name] = deployment
-		for _, deployInfo := range deployInfos {
-			newDeployments = append(newDeployments, deployInfo)
-		}
-	} else {
-		newDeployments = append(newDeployments, deployment)
-		fileDeployment.Deployments = newDeployments
+		Deployments: deployments,
 	}
 
 	if err := common.WriteObjectToFile(file.Path, fileDeployment); err != nil {
@@ -63,37 +49,29 @@ func (file *File) StoreNewDeployment(deployment *StoreDeployment) error {
 
 func (file *File) LoadDeployment() ([]*StoreDeployment, error) {
 	fileDeployment := &FileDeployment{}
-	if err := common.LoadFileToObject(file.Path, fileDeployment); err != nil {
-		return nil, fmt.Errorf("Unable to load deployment status: %s", err.Error())
+	if _, err := os.Stat(file.Path); err == nil {
+		if err := common.LoadFileToObject(file.Path, fileDeployment); err != nil {
+			return nil, fmt.Errorf("Unable to load deployment status: %s", err.Error())
+		}
 	}
 
 	return fileDeployment.Deployments, nil
 }
 
 func (file *File) DeleteDeployment(deploymentName string) error {
-	fileDeployment := &FileDeployment{
-		Deployments: []*StoreDeployment{},
+	deployInfos, err := file.getDeployInfos()
+	if err != nil {
+		return fmt.Errorf("Unable to get deployment info: %s", err.Error())
 	}
+	delete(deployInfos, deploymentName)
 
-	if _, err := os.Stat(file.Path); err == nil {
-		deployments, err := file.LoadDeployment()
-		if err != nil {
-			return fmt.Errorf("Unable to load deployment status: %s", err.Error())
-		}
-		fileDeployment.Deployments = deployments
-	}
-
-	deployInfos := map[string]*StoreDeployment{}
-	for _, deployment := range fileDeployment.Deployments {
-		if deployment.Name == deploymentName {
-			continue
-		}
-		deployInfos[deployment.Name] = deployment
-	}
-
-	newDeployments := []*StoreDeployment{}
+	deployments := []*StoreDeployment{}
 	for _, deployInfo := range deployInfos {
-		newDeployments = append(newDeployments, deployInfo)
+		deployments = append(deployments, deployInfo)
+	}
+
+	fileDeployment := &FileDeployment{
+		Deployments: deployments,
 	}
 
 	if err := common.WriteObjectToFile(file.Path, fileDeployment); err != nil {
@@ -101,4 +79,20 @@ func (file *File) DeleteDeployment(deploymentName string) error {
 	}
 
 	return nil
+}
+
+func (file *File) getDeployInfos() (map[string]*StoreDeployment, error) {
+	deployInfos := map[string]*StoreDeployment{}
+	if _, err := os.Stat(file.Path); err == nil {
+		deployments, err := file.LoadDeployment()
+		if err != nil {
+			return nil, fmt.Errorf("Unable to load deployment status: %s", err.Error())
+		}
+
+		for _, deployment := range deployments {
+			deployInfos[deployment.Name] = deployment
+		}
+	}
+
+	return deployInfos, nil
 }
