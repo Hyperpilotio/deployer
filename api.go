bd5394aa14c84a18f24602387eab01a47f3fbc82
LinusHung <linushung0325@gmail.com>
2017-04-28 09:55:49 +0800

    Fix  [UI]issue#42
    Add “.log” extension

--- api.go
+++ api.go
@@ -770,7 +770,7 @@ func (server *Server) logUI(c *gin.Context) {
 
 func (server *Server) getDeploymentLog(c *gin.Context) {
 	logFile := c.Param("logFile")
-	logPath := path.Join(server.Config.GetString("filesPath"), "log", logFile)
+	logPath := path.Join(server.Config.GetString("filesPath"), "log", logFile+".log")
 	file, err := os.Open(logPath)
 	if err != nil {
 		c.JSON(http.StatusNotFound, gin.H{
