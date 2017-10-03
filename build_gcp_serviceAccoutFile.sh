ec0655afd4d7d0e9b2b8be7e3c0ea73e107c98f5
alan.yang <jpra1113@gmail.com>
2017-10-03 09:46:34 +0800

    Add some use description

--- build_gcp_serviceAccoutFile.sh
+++ build_gcp_serviceAccoutFile.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+# Use GCP web console or gcloud create projectId before run
+# System will auto create 'compute Engine default service account' when you first click 'Container Engine' from GCP web console
+# gcloud init
 echo "Building GCP Service account JSON File"
 gcloud iam service-accounts keys create \
     ~/gcpServiceAccount.json \
     --iam-account $(gcloud iam service-accounts list | grep "Compute Engine default service account" | awk '{print $6}')
-
